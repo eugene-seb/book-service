3dcf0e49e9bce9723a1eec66eb63498baf14f5ac
eugene-seb <etoundisebastien@gmail.com>
2025-08-17 16:41:16 +0200

    Refactor BookSpecification to use AND conjunction for attribute filters; update CategoryRepository accordingly

--- src/main/java/com/eugene/book_service/repository/CategoryRepository.java
+++ src/main/java/com/eugene/book_service/repository/CategoryRepository.java
@@ -2,15 +2,11 @@ package com.eugene.book_service.repository;
 
 import com.eugene.book_service.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface CategoryRepository
         extends JpaRepository<Category, Long>
 {
-    
-    @Query("SELECT c FROM Category c WHERE c.name = :categoryName")
-    Optional<Category> findByName(@Param("categoryName") String categoryName);
+    Optional<Category> findByName(String categoryName);
 }
--- src/main/java/com/eugene/book_service/repository/specification/BookSpecification.java
+++ src/main/java/com/eugene/book_service/repository/specification/BookSpecification.java
@@ -2,11 +2,7 @@ package com.eugene.book_service.repository.specification;
 
 import com.eugene.book_service.dto.BookDto;
 import com.eugene.book_service.model.Book;
-import jakarta.persistence.criteria.Predicate;
 import org.springframework.data.jpa.domain.Specification;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class BookSpecification
 {
@@ -15,26 +11,38 @@ public class BookSpecification
     }
     
     public static Specification<Book> filterBy(BookDto bookDto) {
-        return (root, query, criteriaBuilder) -> {
-            List<Predicate> predicates = new ArrayList<>();
-            
-            if (bookDto.getIsbn() != null) predicates.add(criteriaBuilder.like(root.get("isbn"),
-                                                                               "%" +
-                                                                               bookDto.getTitle() +
-                                                                               "%"));
-            if (bookDto.getTitle() != null) predicates.add(criteriaBuilder.like(root.get("title"),
-                                                                                "%" +
-                                                                                bookDto.getTitle() +
-                                                                                "%"));
-            if (bookDto.getDescription() != null)
-                predicates.add(criteriaBuilder.like(root.get("description"),
-                                                    "%" + bookDto.getDescription() + "%"));
-            if (bookDto.getAuthor() != null) predicates.add(criteriaBuilder.like(root.get("author"),
-                                                                                 "%" +
-                                                                                 bookDto.getAuthor() +
-                                                                                 "%"));
-            
-            return criteriaBuilder.or(predicates.toArray(new Predicate[0]));
-        };
+        return Specification
+                .where(filterByIsbn(bookDto.getIsbn()))
+                .or(filterByTitle(bookDto.getTitle()))
+                .or(filterByDescription(bookDto.getDescription()))
+                .or(filterByAuthor(bookDto.getAuthor()));
+    }
+    
+    public static Specification<Book> filterByIsbn(String isbn) {
+        return ((root, query, criteriaBuilder) -> isbn != null
+                ? criteriaBuilder.like(root.get("isbn"),
+                                       "%" + isbn + "%")
+                : criteriaBuilder.conjunction());
+    }
+    
+    public static Specification<Book> filterByTitle(String title) {
+        return ((root, query, criteriaBuilder) -> title != null
+                ? criteriaBuilder.like(root.get("title"),
+                                       "%" + title + "%")
+                : criteriaBuilder.conjunction());
+    }
+    
+    public static Specification<Book> filterByDescription(String description) {
+        return ((root, query, criteriaBuilder) -> description != null
+                ? criteriaBuilder.like(root.get("description"),
+                                       "%" + description + "%")
+                : criteriaBuilder.conjunction());
+    }
+    
+    public static Specification<Book> filterByAuthor(String author) {
+        return ((root, query, criteriaBuilder) -> author != null
+                ? criteriaBuilder.like(root.get("author"),
+                                       "%" + author + "%")
+                : criteriaBuilder.conjunction());
     }
 }
